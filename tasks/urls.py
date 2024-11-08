ff2ea51ba99f4f7743f076bf37b76c934ee89c0b
mizou9494 <betarmizou@gmail.com>
2024-11-08 22:53:38 +0100

    -

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+# the app name is because we have that one route named index
+app_name = "tasks"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("add", views.add, name="add"),
+    path("add/", views.add, name="add"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-tasks = [ "shop", "work", "pray", "eat"]
+tasks = [ "shop", "work", "pray", "eat", "sleep" ]	
 # Create your views here.
 def index(request):
-    # now = datetime.datetime.now()
+    # now we will pass those tasks in an array as an argument and catch them in the html file
     return render(request, "tasks/index.html", {
         "tasks": tasks
     })
