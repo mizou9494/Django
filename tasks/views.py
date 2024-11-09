7817320c0a893b73e8a6396de38dd6b783041661
mizou9494 <betarmizou@gmail.com>
2024-11-09 18:18:22 +0100

    we learned how to do client side and server side validation

--- tasks/views.py
+++ tasks/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import render
 tasks = [ "shop", "work", "pray", "eat", "sleep" ]	
 
 class NewTaskForm(forms.Form):
-    task = forms.CharField(label="New Task")
+    task = forms.CharField(label="Add New Task")
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=8)
+
 # Create your views here.
 def index(request):
     # now we will pass those tasks in an array as an argument and catch them in the html file
@@ -14,6 +16,17 @@ def index(request):
     })
 
 def add(request):
+    if request.method == "POST":
+        # validating the data in the server side
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+
     return render(request, "tasks/add.html", {
         "form": NewTaskForm()
     })
